08f022d060532b979d3f7f6794f90bff7199816c
Leninist <58590858+celtrecium@users.noreply.github.com>
2020-04-25 17:55:17 +0500

    Add files via upload

--- drawlib.h
+++ drawlib.h
@@ -17,26 +17,28 @@ namespace drwlib{
 	
 	/* Draw rectangle */
 	
-	void draw_rect(int rect_h_0, int rect_w_0, int rect_h_1, int rect_w_1, int rect_colour_fg, int rect_colour_bg){
-	        int delta_r_h{rect_h_1 - rect_h_0};
+	void draw_rect(int rect_w_0, int rect_h_0, int rect_w_1, int rect_h_1, int rect_colour_fg, int rect_colour_bg){
+		int delta_r_h{rect_h_1 - rect_h_0};
 	        int delta_r_w{rect_w_1 - rect_w_0};
-	        std::string buff;
-	        for(int count_1{0}; count_1 <= delta_r_h; count_1++){
-	                int cprw{count_1 + rect_h_0};
-	                for(int count_2{0}; count_2 <= delta_r_w; count_2++){
-	                        int cprh{count_2 + rect_w_0};
-	                        buff += "\033[" + std::to_string(cprh) + ";" + std::to_string(cprw) + "H" + "\033[" + std::to_string(rect_colour_bg) + ";" + std::to_string(rect_colour_fg) + "m ";
-	                }
-	        }
-	        std::cout << buff << std::flush;
+		std::string spaces;
+		std::string spaces_1;
+		for(int count{0}; count <= delta_r_w; ++count){
+			spaces += " ";
+		}
+		for(int count{0}; count <= delta_r_h; ++count){
+			int cprh{rect_h_0 + count};
+			spaces_1 += "\033[" + std::to_string(cprh) + ";" + std::to_string(rect_w_0) + "H" + spaces;
+		}
+		std::string buff{"\033[" + std::to_string(rect_colour_bg) + ";" + std::to_string(rect_colour_fg) + "m" + spaces_1};
+		std::cout << buff << std::flush;
 	}
-	
-	/* Go to coordinates X, Y */
-	
+
+	/* Go to cordinates X, Y */
+
 	void gotoxy(int x, int y){
 	        std::cout << "\033[" << y << ";" << x << "H" << std::flush;
 	}
-	
+
 	/* Draw simpe button with text */
 	
 	void draw_button(int button_y, int button_x, std::string text, int bg, int fg){
